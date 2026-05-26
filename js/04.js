b01f4dc9bcd9278e1b08775ffc53eb969d53f343
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-05-26 05:29:16 -0600

    Add files via upload

---
+++ js/04.js
@@ -0,0 +1,10 @@
+const producto ="monitor de 20 pulgadas";
+
+const producto2 =String("monitor de 20 pulgadas");
+const producto3 = new String("monitor de 20 pulgadas");
+
+
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
