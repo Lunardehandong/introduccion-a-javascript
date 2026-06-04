66a8ef0f88950bd5f625339cb37ff05928536444
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-04 13:14:54 -0600

    Update 09.js

--- js/09.js
+++ js/09.js
@@ -1,1 +1,18 @@
+// 1. Declaración de un comentario de línea para organizar el código bajo el tema de "Booleans"
+// Booleans
 
+// 2. Línea en blanco para mantener una estructura limpia y legible
+
+// 3. Crea una constante llamada 'boolean1' y le asigna el valor booleano verdadero (true)
+const boolean1 = true;
+
+// 4. Crea una constante llamada 'boolean2' y le asigna el valor booleano falso (false)
+const boolean2 = false;
+
+// 5. Línea en blanco para separar la declaración de variables de la salida de datos
+
+// 6. Imprime en la consola del navegador o terminal el valor almacenado en 'boolean1' (true)
+console.log(boolean1);
+
+// 7. Imprime en la consola del navegador o terminal el valor almacenado en 'boolean2' (false)
+console.log(boolean2);
