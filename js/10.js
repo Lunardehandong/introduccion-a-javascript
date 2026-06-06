4cf7cc67892f4faadffa238a4e58db7914eebe13
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-06 15:03:06 -0600

    Add product object with properties and console log

--- js/10.js
+++ js/10.js
@@ -1,1 +1,13 @@
+const producto ={
+    nombre:"monitor de 20 pulgadas",
+    precio: 300,
+    disponible=true
+}
+
+console.log(producto);
+
+//console.log(producto.precio);
+//console.log(producto.nombre);
+//console.log(producto.disponible);
+//console.log(producto["precio"]);
 
