7672aef2bd26ba9f85185c6130abedd3cec9105b
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-09 16:34:23 -0600

    Add array examples and logging in 13.js

---
+++ js/13.js
@@ -0,0 +1,26 @@
+// Arreglos o Arrays
+
+const numeros = [10,20,30,40,50];
+console.table(numeros);
+
+
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+console.table(meses);
+
+
+
+// Acceder a los valores de un arreglo
+console.log(numeros[0]);
+console.log(numeros[1]);
+console.log(numeros[2]);
+console.log(numeros[3]);
+console.log(numeros[4]);
+
+
+// Conocer la extensión de un arreglo
+console.log(meses.length);
+
+numeros.forEach( function(numero) {
+    console.log(numero);
+})
