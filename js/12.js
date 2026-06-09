d1764c70b1aa4beee774ee3343bbff2ecd6614ef
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-08 20:43:51 -0600

    Modify product object price and delete property
    Updated product price and deleted the price property.

--- js/12.js
+++ js/12.js
@@ -1,1 +1,13 @@
+//"use strict";
+const producto ={
+    nombreproducto:"monitor de 20 pulgadas",
+    precio: 300,
+    disponible: true
 
+}
+
+Object.seal(producto);
+producto.precio='nuevo precio';
+delete producto.precio;
+
+console.log(producto);
