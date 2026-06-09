41bacf84e3f0f38775e0cf36d49d4df5859e01e5
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-08 20:36:05 -0600

    Add product object and destructure properties

--- js/11.js
+++ js/11.js
@@ -1,1 +1,13 @@
 
+const producto ={
+    nombreproducto:"monitor de 20 pulgadas",
+    precio: 300,
+    disponible: true
+
+}
+
+//destructuring
+
+const{ precio, nombreproducto} =producto;
+console.log(precio);
+console.log(nombreproducto);
