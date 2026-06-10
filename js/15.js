a1d4c565d91a5eb9e80b38b2051b467c19090642
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-10 15:36:07 -0600

    Enhance array examples with push, unshift, and spread
    Added examples of array manipulation including adding elements and using the spread operator.

--- js/15.js
+++ js/15.js
@@ -1,1 +1,25 @@
+// Arreglos o Arrays
 
+const numeros = [10, 20, 30, 40, 50];
+console.table(numeros);
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+console.table(meses);
+
+// Acceder a los valores de un arreglo
+
+// Agregar elementos
+numeros.push(60, 70, 80); // Al final del arreglo
+numeros.unshift(-10, -20, -30); // Al inicio del arreglo
+
+console.table(numeros);
+
+// Métodos de eliminación (Comentados)
+// meses.pop(); // elimina el último elemento
+// meses.shift(); // elimina el primer elemento
+// meses.splice(2, 1);
+// console.table(meses);
+
+// Rest Operator o Spread Operator
+const nuevoArreglo = [...meses, 'Junio'];
+console.log(nuevoArreglo);
