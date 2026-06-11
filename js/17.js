759508d155d982dc97cad996d4a1ddc86d031b4d
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-11 16:03:53 -0600

    Add function declarations and expressions for summation

--- js/17.js
+++ js/17.js
@@ -1,1 +1,14 @@
+// Declaración de Función
+function sumar() {
+    console.log(10 + 10);
+}
 
+sumar();
+
+
+// Expresión de la función
+const sumar2 = function() {
+    console.log(3 + 3);
+}
+
+sumar2();
