f783d1c06567cfd46eb57c084b706ec11c1ac7f7
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-16 16:19:48 -0600

    Add arrow functions for summation and learning

--- js/22.js
+++ js/22.js
@@ -1,1 +1,9 @@
+// Arrow Functions
 
+const sumar2 = (n1, n2) => console.log( n1 + n2);
+sumar2(5, 10);
+
+
+const aprendiendo = tecnologia => console.log(`Aprendiendo ${tecnologia}`)
+
+aprendiendo('JavaScript')
