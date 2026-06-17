2b3a47b0cda62cad5740e0f2ee79e11a422ed042
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-17 17:33:42 -0600

    Update 23.js

--- js/23.js
+++ js/23.js
@@ -1,3 +1,11 @@
+//const puntaje = 100;
+
+//if(puntaje !== 1000) {
+   // console.log('No es igual');
+//} else {
+    //console.log('Si el puntaje es 1000');
+//}
+
 // const carrito = 800;
 
 // if( efectivo > carrito ) {
