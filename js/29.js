7499b805a1c0417ebeba85999e4046af148c3eb5
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-06-30 12:50:34 -0600

    Add Libro class extending Producto with formatting

--- js/29.js
+++ js/29.js
@@ -16,3 +16,20 @@ const producto3 = new Producto('Laptop', 300);
 
 console.log(producto2);
 console.log(producto3);
+
+// Herencia
+
+class Libro extends Producto {
+    constructor(nombre, precio, isbn) {
+        super(nombre, precio);
+        this.isbn = isbn;
+    }
+
+    formatearProducto() {
+        return `${super.formatearProducto() } y su ISBN es ${this.isbn}`;
+    }
+}
+
+const libro = new Libro('JavaScript la Revolución', 120, '910313981389139');
+console.log(libro.formatearProducto() );
+console.log(producto2.formatearProducto());
