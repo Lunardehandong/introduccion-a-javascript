2e3264dff2fe13968afb8f25e6f990a78a12cfb1
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-07-01 14:53:14 -0600

    Update 30.js

--- js/30.js
+++ js/30.js
@@ -1,1 +1,12 @@
+const numero1 = 20;
+const numero3 = 30;
 
+console.log(numero1);
+
+try {
+    console.log(numero2); // Intenta mandar a llamar una variable que no existe
+} catch (error) {
+    console.log(error);   // Captura el error de referencia y lo muestra sin detener el script
+}
+
+console.log(numero3);
