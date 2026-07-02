0061e1b739442c9d0f7dc2b3b0f1fc0d23547fdc
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-07-02 13:27:08 -0600

    Implement user authentication with Promise
    Added a Promise to handle user authentication with resolve and reject cases.

--- js/31.js
+++ js/31.js
@@ -1,1 +1,20 @@
+// Creación de una Promise
+const usuarioAutenticado = new Promise( (resolve, reject) => {
+    const auth = true;
 
+    if(auth) {
+        resolve('Usuario Autenticado'); // EL PROMISE SE CUMPLE
+    } else {
+        reject('No se pudo iniciar sesión'); // El promise no se cumple
+    }
+});
+
+// Consumo de la Promise
+usuarioAutenticado
+    .then( (resultado) => console.log(resultado) )
+    .catch( (error) => console.log(error) )
+
+// En los Promises existen 3 valores posibles:
+// Pending   : No se ha cumplido pero tampoco se ha rechazado (está en espera).
+// Fulfilled : Ya se cumplió exitosamente (se ejecuta el .then).
+// Rejected  : Se ha rechazado o no se pudo cumplir (se ejecuta el .catch).
